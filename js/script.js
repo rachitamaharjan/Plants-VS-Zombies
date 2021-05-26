0d4ec170bd563c4b452dd0fa7c491c696b482194
smilerac <smilerac15@gmail.com>
2021-05-26 15:18:32 +0545

    created game board

--- js/script.js
+++ js/script.js
@@ -1,91 +1,182 @@
-var keys = {
-    Space : false,
+var mouseActivity = {
+    MouseHover : false,
+    x : 1, 
+    y : 1,
+    height : 0.1,
+    width : 0.1
 }
 
-var bird = {
-    goingUp : false,
-    goingDown : false
+// var bird = {
+//     goingUp : false,
+//     goingDown : false
+// }
+var canvas = document.getElementById('game')
+var containerPos = (document.getElementsByClassName('main-container-wrapper'))[0]
+var ctx = canvas.getContext('2d')
+
+console.log('k', containerPos)
+console.log('k', canvas.getBoundingClientRect())
+var gridSize = 24;
+var controlPanel = {
+    height : gridSize,
+    width : canvas.width
 }
+var gameBoard = [];
 
-var player = { playing: false, score: 0, speed: 3, gravity: 0.5, velocity: 0, upward: -10, highScore: getHighScore()}
-var pointsBefore = player.score
-var score = document.getElementById('score')
-var popUp = document.getElementsByClassName('pop-up')
-var base = document.getElementById('base')
-var playerBird =  document.createElement('div')
-playerBird.className = 'player-bird'
-playerBird.y = playerBird.offsetTop
-var gameArea = document.getElementById('game-area')
-var blockerPipes = document.getElementsByClassName('blocker-pipes')
-
-
-var startGame = popUp[0].addEventListener('click', function(){
-
-    popUp[0].id = 'invisible'
-    base.style.display = 'none'
-    player.speed = 5
-    gameArea.innerHTML = ''
-    player.playing = true
-    player.score = 0
-    createPipeRow('pipe-row1')
-    createPipeRow('pipe-row2')
-    createPipeRow('pipe-row3')
-
-    mainContainer[0].appendChild(playerBird)
-    playerBird.style.transform = "unset"
-    window.requestAnimationFrame(loop);
+// var mousemove = document.addEventListener('mousemove',function(e){
+canvas.addEventListener('mousemove',function(e){
+    e.preventDefault();
+    mouseActivity[e.MouseHover] = true
+    // mouseActivity.x = e.clientX
+    // mouseActivity.y = e.clientY
+    mouseActivity.x = e.x - containerPos.getBoundingClientRect().left - 30
+    mouseActivity.y = e.y - containerPos.getBoundingClientRect().top - 30
+    console.log('move',e.clientX,e.clientY)
+    // console.log(e.key)
 })
 
-// click and press event listeners
-mainContainer[0].addEventListener('click', flapUp)
-document.addEventListener('keypress', flapUp)
+// var mouseleave = document.addEventListener('mouseleave',function(e){
+canvas.addEventListener('mouseleave',function(e){
+    e.preventDefault();
+    mouseActivity[e.MouseHover] = false
+    mouseActivity.x = undefined
+    mouseActivity.y = undefined
+    console.log('not')
 
-function flapUp(e){
-    bird.goingUp = true
-    bird.goingDown = false
-    document.getElementsByClassName('player-bird')[0].classList.add('bird-down')
-    document.getElementsByClassName('player-bird')[0].classList.remove('bird-up')
-    document.getElementsByClassName('player-bird')[0].classList.remove('bird-down')
-    playerBird.y = playerBird.offsetTop
-    player.velocity += player.upward
-    playerBird.y += player.velocity
-    playerBird.style.top =  playerBird.y + 'px'
-}
+    // console.log(e.key)
+})
 
-function loop(){
+// var player = { playing: false, score: 0, speed: 3, gravity: 0.5, velocity: 0, upward: -10, highScore: getHighScore()}
+// var pointsBefore = player.score
+// var score = document.getElementById('score')
+// var popUp = document.getElementsByClassName('pop-up')
+// var base = document.getElementById('base')
+// var playerBird =  document.createElement('div')
+// playerBird.className = 'player-bird'
+// playerBird.y = playerBird.offsetTop
+// var gameArea = document.getElementById('game-area')
+// var blockerPipes = document.getElementsByClassName('blocker-pipes')
 
-    var blockerPipes = document.getElementsByClassName('blocker-pipes')
-    
-    if (player.playing){
-        window.requestAnimationFrame(loop);
-        moveBase()
-        birdGravity()
-        movePipes('pipe-row1',blockerPipes[0].x)
-        movePipes('pipe-row2',blockerPipes[1].x)
-        movePipes('pipe-row3',blockerPipes[2].x)
 
-        if(bird.goingUp == true){
-            document.getElementsByClassName('player-bird')[0].classList.add('bird-up')
-            document.getElementsByClassName('player-bird')[0].classList.remove('bird-down')
-            // document.getElementsByClassName('player-bird')[0].backgroundImage = '../assets/yellowbird-upflap.png'
+// var startGame = popUp[0].addEventListener('click', function(){
+
+//     // popUp[0].id = 'invisible'
+//     // base.style.display = 'none'
+//     // player.speed = 5
+//     // gameArea.innerHTML = ''
+//     // player.playing = true
+//     // player.score = 0
+//     // createPipeRow('pipe-row1')
+//     // createPipeRow('pipe-row2')
+//     // createPipeRow('pipe-row3')
+
+//     // mainContainer[0].appendChild(playerBird)
+//     // playerBird.style.transform = "unset"
+//     window.requestAnimationFrame(loop);
+// })
+
+// // click and press event listeners
+// mainContainer[0].addEventListener('click', flapUp)
+// document.addEventListener('keypress', flapUp)
+
+// function flapUp(e){
+//     bird.goingUp = true
+//     bird.goingDown = false
+//     document.getElementsByClassName('player-bird')[0].classList.add('bird-down')
+//     document.getElementsByClassName('player-bird')[0].classList.remove('bird-up')
+//     document.getElementsByClassName('player-bird')[0].classList.remove('bird-down')
+//     playerBird.y = playerBird.offsetTop
+//     player.velocity += player.upward
+//     playerBird.y += player.velocity
+//     playerBird.style.top =  playerBird.y + 'px'
+// }
+
+function eachGrid(x, y){
+    this.height = gridSize;
+    this.width = gridSize;
+    this.x = x;
+    this.y = y;
+
+    this.draw = function(){
+        if(mouseActivity.x && mouseActivity.y && checkCollision(this, mouseActivity)){
+            console.log('yes')
+            ctx.strokeStyle = 'gray';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
         }
-        else if(bird.goingDown == true){
-            document.getElementsByClassName('player-bird')[0].classList.add('bird-down')
-            document.getElementsByClassName('player-bird')[0].classList.remove('bird-up')
-            document.getElementsByClassName('player-bird')[0].backgroundImage = '../assets/yellowbird-downflap.png'
-        }
-        else{
-            document.getElementsByClassName('player-bird')[0].classList.add('bird-down')
-            document.getElementsByClassName('player-bird')[0].classList.remove('bird-up')
-            document.getElementsByClassName('player-bird')[0].classList.remove('bird-down')
-        }
-
-        score.innerHTML = 'Score: ' + player.score + '<br/> Speed: ' + player.speed
-
-        if (player.score >= pointsBefore + 20){
-            player.speed += 1
-            pointsBefore = player.score
-        }
-
     }
 }
+
+
+
+(function createGrid(){
+    for(i = gridSize; i < canvas.height; i += gridSize){
+        // for(j = 0; j < canvas.width; j += gridSize){
+        for(j = gridSize * 2.6; j < canvas.width; j += gridSize){
+            // console.log('in')
+            gameBoard.push(new eachGrid(j, i))
+        }
+    }
+})()
+
+function drawGrid(){
+    for(i = 0; i < gameBoard.length; i++){
+        gameBoard[i].draw()
+    }
+}
+
+
+
+function checkCollision(one, two){
+    // one = val1.getBoundingClientRect()
+    // two = val2.getBoundingClientRect()
+    // console.log('l',one,two)
+    
+    if (!((one.x > two.x + two.width) || (one.y > two.y + two.height) || (one.x + one.width < two.x) || (one.y + one.height < two.y))){
+        console.log('this',one.x,one.y,'mouse',two.x,two.y)
+        return 1
+    }
+    else return 0
+}
+
+(function loop(){
+    // console.log('ok')
+
+    // var blockerPipes = document.getElementsByClassName('blocker-pipes')
+    
+    // if (player.playing){
+        ctx.clearRect(0,0,canvas.width, canvas.height)
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(0,0,controlPanel.width,controlPanel.height)
+        drawGrid()
+        window.requestAnimationFrame(loop);
+    //     moveBase()
+    //     birdGravity()
+    //     movePipes('pipe-row1',blockerPipes[0].x)
+    //     movePipes('pipe-row2',blockerPipes[1].x)
+    //     movePipes('pipe-row3',blockerPipes[2].x)
+
+    //     if(bird.goingUp == true){
+    //         document.getElementsByClassName('player-bird')[0].classList.add('bird-up')
+    //         document.getElementsByClassName('player-bird')[0].classList.remove('bird-down')
+    //         // document.getElementsByClassName('player-bird')[0].backgroundImage = '../assets/yellowbird-upflap.png'
+    //     }
+    //     else if(bird.goingDown == true){
+    //         document.getElementsByClassName('player-bird')[0].classList.add('bird-down')
+    //         document.getElementsByClassName('player-bird')[0].classList.remove('bird-up')
+    //         document.getElementsByClassName('player-bird')[0].backgroundImage = '../assets/yellowbird-downflap.png'
+    //     }
+    //     else{
+    //         document.getElementsByClassName('player-bird')[0].classList.add('bird-down')
+    //         document.getElementsByClassName('player-bird')[0].classList.remove('bird-up')
+    //         document.getElementsByClassName('player-bird')[0].classList.remove('bird-down')
+    //     }
+
+    //     score.innerHTML = 'Score: ' + player.score + '<br/> Speed: ' + player.speed
+
+    //     if (player.score >= pointsBefore + 20){
+    //         player.speed += 1
+    //         pointsBefore = player.score
+    //     }
+
+    // }
+})()
--- js/utility.js
+++ js/utility.js
@@ -1,141 +1,141 @@
 
-var range = 5
-var baseX = 0
-var mainContainer = document.getElementsByClassName('main-container')
+// var range = 5
+// var baseX = 0
+// var mainContainer = document.getElementsByClassName('main-container')
 
 
-function createPipeRow(classname){
+// function createPipeRow(classname){
 
-    for(i = 0; i < 2; i++){
-        var blockerPipes = document.createElement('div')
-        blockerPipes.className = 'blocker-pipes'
-        blockerPipes.classList.add(classname)
-        var height = Math.floor((Math.random() * mainContainer[0].offsetHeight/3 )+ 50)
-        blockerPipes.style.height = height + 'px'
-    if (i % 2 == 0){
-        blockerPipes.y = 0
-        blockerPipes.classList.add('pipe-up')
-    }
-    else{
-        blockerPipes.classList.add('pipe-down')
-        blockerPipes.y = mainContainer[0].offsetHeight - parseInt(blockerPipes.style.height)
-    }
-    blockerPipes.style.top = blockerPipes.y + 'px'
-    gameArea.appendChild(blockerPipes) 
-    }
-}
+//     for(i = 0; i < 2; i++){
+//         var blockerPipes = document.createElement('div')
+//         blockerPipes.className = 'blocker-pipes'
+//         blockerPipes.classList.add(classname)
+//         var height = Math.floor((Math.random() * mainContainer[0].offsetHeight/3 )+ 50)
+//         blockerPipes.style.height = height + 'px'
+//     if (i % 2 == 0){
+//         blockerPipes.y = 0
+//         blockerPipes.classList.add('pipe-up')
+//     }
+//     else{
+//         blockerPipes.classList.add('pipe-down')
+//         blockerPipes.y = mainContainer[0].offsetHeight - parseInt(blockerPipes.style.height)
+//     }
+//     blockerPipes.style.top = blockerPipes.y + 'px'
+//     gameArea.appendChild(blockerPipes) 
+//     }
+// }
 
-function birdGravity(){
+// function birdGravity(){
 
-    setTimeout(function(){
-        bird.goingDown = true
-        bird.goingUp = false
-        player.velocity += player.gravity
-        playerBird.y += player.velocity
-        playerBird.style.top = playerBird.y + 'px'
+//     setTimeout(function(){
+//         bird.goingDown = true
+//         bird.goingUp = false
+//         player.velocity += player.gravity
+//         playerBird.y += player.velocity
+//         playerBird.style.top = playerBird.y + 'px'
 
-        if(playerBird.y > mainContainer[0].offsetHeight){
-            playerBird.y = parseInt(mainContainer[0].offsetHeight)
-            player.velocity = 0
-            playerBird.style.top = (parseInt(mainContainer[0].offsetHeight) - playerBird.offsetHeight) + 'px'
-            playerBird.style.transform = "rotate(90deg)"
-            gameOver()
-        }
+//         if(playerBird.y > mainContainer[0].offsetHeight){
+//             playerBird.y = parseInt(mainContainer[0].offsetHeight)
+//             player.velocity = 0
+//             playerBird.style.top = (parseInt(mainContainer[0].offsetHeight) - playerBird.offsetHeight) + 'px'
+//             playerBird.style.transform = "rotate(90deg)"
+//             gameOver()
+//         }
 
-        if(playerBird.y < 0){
-            playerBird.y = 0
-            player.velocity = 0
-        }
-    },300)
-}
-
-
-function moveBase(){
-    // var base = document.getElementById('base')
-    // console.log('base.x',base.getBoundingClientRect().right)
-    // if(parseInt(base.style.right) < 200){
-    //     var temp = parseInt(base.style.right)
-    //     temp += 850
-    //     base.style.right = temp + 'px'
-    //     console.log('ok',base.style.right)
-    // }
-    // baseX += player.speed
-    // base.style.right = baseX + 'px'
-    // base.style.right = base.x + 'px'
-    // console.log('base.x',player.speed,baseX,base.offsetWidth)
-
-}
+//         if(playerBird.y < 0){
+//             playerBird.y = 0
+//             player.velocity = 0
+//         }
+//     },300)
+// }
 
 
-var movement = mainContainer[0].offsetWidth
+// function moveBase(){
+//     // var base = document.getElementById('base')
+//     // console.log('base.x',base.getBoundingClientRect().right)
+//     // if(parseInt(base.style.right) < 200){
+//     //     var temp = parseInt(base.style.right)
+//     //     temp += 850
+//     //     base.style.right = temp + 'px'
+//     //     console.log('ok',base.style.right)
+//     // }
+//     // baseX += player.speed
+//     // base.style.right = baseX + 'px'
+//     // base.style.right = base.x + 'px'
+//     // console.log('base.x',player.speed,baseX,base.offsetWidth)
 
-function movePipes(classname){
+// }
+
+
+// var movement = mainContainer[0].offsetWidth
+
+// function movePipes(classname){
     
-    var elements = document.querySelectorAll('.' + classname)
-    elements.forEach(element => {
-        element.x = element.offsetLeft
+//     var elements = document.querySelectorAll('.' + classname)
+//     elements.forEach(element => {
+//         element.x = element.offsetLeft
 
-        if(checkCollision(playerBird, element)){
-            gameOver()
-        }
+//         if(checkCollision(playerBird, element)){
+//             gameOver()
+//         }
        
-        if(isPassingOver(element)){
-            player.score = player.score + 1
-        }
+//         if(isPassingOver(element)){
+//             player.score = player.score + 1
+//         }
 
-        if(element.x < 0){
-            element.x += 1700
-            element.style.height = Math.floor((Math.random() * mainContainer[0].offsetHeight/3 )+ 50)
-        }
+//         if(element.x < 0){
+//             element.x += 1700
+//             element.style.height = Math.floor((Math.random() * mainContainer[0].offsetHeight/3 )+ 50)
+//         }
 
-        element.x -= player.speed
-        element.style.left = element.x + 'px'
-    });
-}
+//         element.x -= player.speed
+//         element.style.left = element.x + 'px'
+//     });
+// }
 
-function checkCollision(bird, pipe){
-    birdPos = bird.getBoundingClientRect()
-    pipePos = pipe.getBoundingClientRect()
+// function checkCollision(bird, pipe){
+//     birdPos = bird.getBoundingClientRect()
+//     pipePos = pipe.getBoundingClientRect()
     
-    if ((birdPos.top > pipePos.bottom) || (birdPos.bottom < pipePos.top) || (birdPos.right < pipePos.left) || (birdPos.left > pipePos.right)){
-        return 0
-    }
-    else return 1
-}
+//     if ((birdPos.top > pipePos.bottom) || (birdPos.bottom < pipePos.top) || (birdPos.right < pipePos.left) || (birdPos.left > pipePos.right)){
+//         return 0
+//     }
+//     else return 1
+// }
 
-function gameOver(){
-    var finalScore = player.score
-    console.log('f',finalScore)
-    player.playing = false
-    popUp[0].id = 'visible'
-    popUp[0].innerHTML = ' <br/> Final score: <span class = "display-key"> '+ finalScore + '</span><br/> High score: <span class = "display-key"> '+ player.highScore + '</span><br/> Click to Play Again!'
-    if (player.score > player.highScore){
-        player.highScore = player.score
-    }
-    storeHighScore()
-    player.score = 0    
-}
+// function gameOver(){
+//     var finalScore = player.score
+//     console.log('f',finalScore)
+//     player.playing = false
+//     popUp[0].id = 'visible'
+//     popUp[0].innerHTML = ' <br/> Final score: <span class = "display-key"> '+ finalScore + '</span><br/> High score: <span class = "display-key"> '+ player.highScore + '</span><br/> Click to Play Again!'
+//     if (player.score > player.highScore){
+//         player.highScore = player.score
+//     }
+//     storeHighScore()
+//     player.score = 0    
+// }
 
-function isPassingOver(element){
-    for(i = 150 - range; i <= 150 + range; i++){
-        if(element.offsetLeft == i){
-            var val = true
-            break
-        }
-    }
-    return val
-}
+// function isPassingOver(element){
+//     for(i = 150 - range; i <= 150 + range; i++){
+//         if(element.offsetLeft == i){
+//             var val = true
+//             break
+//         }
+//     }
+//     return val
+// }
 
-function storeHighScore(){
-    if(player.highScore > window.localStorage.getItem('highScore') ){
-        localStorage.setItem('highScore', player.highScore);
-    }
-    if(window.localStorage.getItem('highScore') == null){
-        localStorage.setItem('highScore', 0);
-    }
+// function storeHighScore(){
+//     if(player.highScore > window.localStorage.getItem('highScore') ){
+//         localStorage.setItem('highScore', player.highScore);
+//     }
+//     if(window.localStorage.getItem('highScore') == null){
+//         localStorage.setItem('highScore', 0);
+//     }
 
-}
+// }
 
-function getHighScore(){
-    return localStorage.getItem('highScore');
-}
+// function getHighScore(){
+//     return localStorage.getItem('highScore');
+// }
